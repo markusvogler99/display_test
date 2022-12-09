a03f6ec3fc4a0046ac0f7bf5f813460aadd1cb8b
Markus Vogler <vm1116@mci4me.at>
2022-12-09 18:46:22 +0100

    123

--- src/main.cpp
+++ src/main.cpp
@@ -1,157 +1,101 @@
 #include <Arduino.h>
-#include <Adafruit_ILI9341.h>
-#include <Adafruit_GFX.h>
 #include "SPI.h"
 #include "HX711.h"
 #include "SD.h"
+#include "Force_Sensor.h"
+#include "Speed_Sensor.h"
+#include "string.h"
+#include "Display_MCI.h"
+#include "elapsedMillis.h"
 
 
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
 #define TFT_CS 10
 
-const int BENDING_FORCE_DOUT_PIN = 2;
-const int BENDING_FORCE_SCK_PIN = 3;
+//Pins definieren für die Hx711, an denen die Kraftsensoren hängen 
+const int BEND_DOUT_PIN = 2;
+const int BEND_SCK_PIN = 3;
 
-const int AXIAL_FORCE_DOUT_PIN = 6;
-const int AXIAL_FORCE_SCK_PIN = 20;
+const int AX_DOUT_PIN = 6;
+const int AX_SCK_PIN = 20;
+
+const int GAIN = 128; 
+
+//Pin für Speed Sensor 
+const int SPEED_DIN_PIN = 5; 
+
+//Definition der Kalibrierdaten für die beiden Kraftsensoren 
+const double slope_bend = 0.000111688; 
+const double offset_bend = 1.05;
+
+const double slope_ax = 0.0001; 
+const double offset_ax = 1;
+
+
 
 
 const int BUTTON = 5;
-int BUTTONstate = 0;
-int count_value =0;
-int prestate =0;
+
 int counter = 0; 
 int column = 0;
+
+
+
+//File myFile;
+ 
+//HX711 bending_force;
+//HX711 axial_force;
+
+Force_Sensor test_bending;
+Force_Sensor test_axial;
+
+Speed_Sensor speed_sensor;
+
+
+ 
+//Force_Sensor
 double reading_bend = 0;
 double reading_ax = 0;
 
-File myFile;
- 
+//Speed_Sensor
 
-HX711 bending_force;
-HX711 axial_force;
-
-elapsedMillis em;
-
-// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-// If using the breakout, change pins as desired
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+double rpm_value = 0; 
 
 
-unsigned long testText() {
- // unsigned long start = micros();
-  tft.setCursor(5, 100);
-  tft.setTextColor(ILI9341_RED);  
-  tft.setTextSize(3);
-  tft.println("Hello World!");
-}
 
-
+Display_MCI Display;
+elapsedMillis time_test;
 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ILI9341 Test!"); 
-  tft.begin();
-  // read diagnostics (optional but can help debug problems)
-  uint8_t x = tft.readcommand8(ILI9341_RDMODE);
-  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9341_RDMADCTL);
-  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9341_RDPIXFMT);
-  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9341_RDIMGFMT);
-  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
-  x = tft.readcommand8(ILI9341_RDSELFDIAG);
-  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
-   tft.fillScreen(ILI9341_WHITE);
   
    pinMode(BUTTON, INPUT);
 
-   bending_force.begin(BENDING_FORCE_DOUT_PIN, BENDING_FORCE_SCK_PIN);
-   axial_force.begin(AXIAL_FORCE_DOUT_PIN, AXIAL_FORCE_SCK_PIN);
+   test_bending.init(BEND_DOUT_PIN,BEND_SCK_PIN,GAIN);
+   test_axial.init(AX_DOUT_PIN,AX_SCK_PIN,GAIN);
+
+    Display.init_display();
 
 }
 
 
 void loop(void) {
-  tft.setRotation(3);
-    //testText();
-
-   BUTTONstate = digitalRead(BUTTON);
-
-  if(em > 5000){
-    em = 0;
-    Serial.println("Test");
-    
+  if (time_test>100){
+  reading_bend = test_bending.get_force_value(slope_bend, offset_bend);
+  reading_ax = test_axial.get_force_value(slope_ax, offset_ax);
+  time_test = 0; 
   }
 
- if (BUTTONstate == HIGH && prestate == 0) {
-     count_value++;
-     prestate = 1;
-     tft.fillScreen(ILI9341_WHITE);
-   } else if(BUTTONstate == LOW) {
-     prestate = 0;
-   }
- 
-  tft.setCursor(5, 5);
-   tft.setTextColor(ILI9341_BLACK);  
-   tft.setTextSize(2);
-   tft.print("Lastzyklen: ");
-   tft.print(count_value);
-
-
-  // set the stroke color to white#
-   tft.setTextSize(2);
-  tft.setTextColor(ILI9341_BLACK);
-  tft.setCursor(5, 100);
-  tft.print("Biegekraft: ");
-   tft.setCursor(140, 100);
-
-  if (bending_force.is_ready()) {
-     reading_bend = bending_force.read()*0.000111688 + 1.05;
-    if (reading_bend < 0)
-    {
-      reading_bend = reading_bend * (-1);
-    }
-    tft.setTextColor(ILI9341_BLACK);
-    tft.printf("%3.2f N",reading_bend); 
-  } else {
-    tft.println("HX711 not found.");
-  }
-
-   tft.setTextSize(2);
-  tft.setTextColor(ILI9341_BLACK);
-  tft.setCursor(5, 120);
-  tft.print("Axialkraft: ");
-   tft.setCursor(140, 120);
-
-  if (axial_force.is_ready()) {
-     reading_ax = axial_force.read()*0.000111688 + 1.05;
-    if (reading_ax < 0)
-    {
-      reading_ax = reading_ax * (-1);
-    }
-    tft.setTextColor(ILI9341_BLACK);
-    tft.printf("%3.2f N",reading_ax); 
-  } else {
-    tft.println("HX711 not found");
-  }
-
-  
-  delay(500);
-  tft.setTextColor(ILI9341_WHITE);
-   tft.setCursor(140, 100);
-   tft.printf("%3.2f N",reading_bend);
-   tft.setTextColor(ILI9341_WHITE);
-   tft.setCursor(140, 120);
-   tft.printf("%3.2f N",reading_ax);
-  
+ rpm_value = speed_sensor.get_rpm_value(SPEED_DIN_PIN);
  
 
+ 
+ Display.draw_display(reading_bend, reading_ax,rpm_value);
 
+ 
 
+  
 }
 
